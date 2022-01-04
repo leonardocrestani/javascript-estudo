22b4f47610fefecdeb574d4dccb3ddc71667a060
Leonardo Crestani <leonardo.crestani@hotmail.com>
2022-01-04 17:56:59 -0300

    Ajustes api sequelize

--- API-Cadastro-MYSQL-Sequelize/api/banco-de-dados/criaTabelas.js
+++ API-Cadastro-MYSQL-Sequelize/api/banco-de-dados/criaTabelas.js
@@ -1,7 +1,11 @@
 const ModeloTabelaCadastro = require('../rotas/pessoas/ModeloTabelaPessoas.js');
+const ModeloTabelaTeste = require('../rotas/pessoas/ModeloTabelaTeste.js');
 
-ModeloTabelaCadastro.sync().then(() => {
-    console.log('Tabela criada com sucesso');
+let promiseTabelaCadastro = ModeloTabelaCadastro.sync();
+let promiseTabelaTeste = ModeloTabelaTeste.sync();
+
+Promise.all([promiseTabelaCadastro, promiseTabelaTeste]).then(() => {
+    console.log("Tabelas 'CADASTRO' e 'TESTE' foram chamadas");
 })
 .catch((erro) => {
     console.log(erro.message);
--- API-Cadastro-MYSQL-Sequelize/api/index.js
+++ API-Cadastro-MYSQL-Sequelize/api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const config = require('config');
 const router = require('./rotas/pessoas/index.js');
+const ModeloTabelaCadastro = require('./rotas/pessoas/ModeloTabelaPessoas.js');
 const InformacoesIncorretas = require('./erros/InformacoesIncorretas.js');
 const UsuarioJaCadastrado = require('./erros/UsuarioJaCadastrado.js');
 const formatosAceitos = require('./Serializador.js').formatosAceitos;
@@ -10,35 +11,43 @@ const formatosAceitos = require('./Serializador.js').formatosAceitos;
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    let formatoRequisitado = req.header('Accept');
-    if(formatoRequisitado === '*/*') {
-        formatoRequisitado = 'application/json'
-    }
-    let existe = formatosAceitos.some((formato) => {
-        return formato === formatoRequisitado;
+let promiseTabelaCadastro = ModeloTabelaCadastro.sync();
+
+Promise.all([promiseTabelaCadastro]).then(() => {
+    console.log("Tabelas 'CADASTRO' e 'TESTE' foram chamadas");
+    app.use((req, res, next) => {
+        let formatoRequisitado = req.header('Accept');
+        if(formatoRequisitado === '*/*') {
+            formatoRequisitado = 'application/json'
+        }
+        let existe = formatosAceitos.some((formato) => {
+            return formato === formatoRequisitado;
+        });
+        if(!existe) {
+            res.status(406);
+            res.end()
+        }
+        else {
+            res.setHeader('Content-Type', formatoRequisitado);
+            next();
+        }
     });
-    if(!existe) {
-        res.status(406);
-        res.end()
-    }
-    else {
-        res.setHeader('Content-Type', formatoRequisitado);
-        next();
-    }
-});
-
-app.use('/acessos', router);
-
-app.use((erro, req, res, next) => {
-    if(erro instanceof InformacoesIncorretas || erro instanceof UsuarioJaCadastrado) {
-        res.status(400);
-    }
-    res.send(JSON.stringify({
-        mensagem: erro.message
-    }));
-});
-
-app.listen(config.get('api.porta'), () => {
-    console.log("Servidor rodando");
+    
+    app.use('/acessos', router);
+    
+    app.use((erro, req, res, next) => {
+        if(erro instanceof InformacoesIncorretas || erro instanceof UsuarioJaCadastrado) {
+            res.status(400);
+        }
+        res.send(JSON.stringify({
+            mensagem: erro.message
+        }));
+    });
+    
+    app.listen(config.get('api.porta'), () => {
+        console.log("Servidor rodando");
+    });
+})
+.catch((erro) => {
+    console.log(erro.message);
 });
